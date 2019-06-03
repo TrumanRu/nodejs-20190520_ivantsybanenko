a59e8a79fc1e6e2c20f43497c49f271da2d26305
ivan.tsybanenko <ivan.tsybanenko@utg.aero>
2019-06-03 20:24:32 +0300

    add solution for 2-module 2-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,8 +1,29 @@
-const stream = require('stream');
+// const stream = require('stream');
+const {Transform} = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
-class LimitSizeStream extends stream.Transform {
+class TransformBase extends Transform {
+  constructor(options) {
+    super(options);
+    if (options.highWaterMark != null) {
+      // something
+    }
+  }
 
+  _transform(chunk, encoding, callback) {
+    throw new Error('Не определён метод _transform');
+  }
+
+  function1() {
+    this._transform();
+  }
+
+  function2() {
+
+  }
+}
+
+class LimitSizeStream extends TransformBase {
   constructor(options) {
     super(options);
     this._limit = (options.limit == null) ? 0 : options.limit;
